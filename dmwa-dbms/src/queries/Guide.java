ee853b2339b84d8f031657df5a4e006cd7454125
Prachi Kabtiyal <pr522601@dal.ca>
2021-11-16 00:30:54 -0400

    feature/query

---
+++ dmwa-dbms/src/queries/Guide.java
@@ -0,0 +1,25 @@
+package queries;
+
+public class Guide {
+    
+    public static void print_queries_for_reference(){
+
+        System.out.println("\n\n\t\t\t*********** Database Queries ************\t\t\t\n");
+
+
+        String database_queries = "Query to: \n\n"
+                        +"1. Create database : create database <database_name>;\n\n"
+                        +"2. Use database : user database <database_name>;\n\n"
+                        +"3. Create table : create table <table_name> (<column1> text, "
+                        +"<column2> int, <column3> float, <column3> date);\n\n"
+                        +"4. Insert value in table: insert : insert into <table_name> values (\"Rita\", 100, 100, 2021-01-01);\n\n"
+                        +"5. Fetch data: select */<coma_separated_column_names> from <table_name> where rank=1;\n\n"
+                        +"6. Update a column: update <table_name> set <column1>=\"Seema\" where <column1>=\"Rita\";\n\n"
+                        +"7. Delete a row: delete from <table_name> where <column1>=\"Rita\";\n\n"
+                        +"8. Drop table: drop table <table_name>;\n\n";
+        
+        System.out.println(database_queries);
+
+    }
+
+}
--- dmwa-dbms/src/queries/QueryDriver.java
+++ dmwa-dbms/src/queries/QueryDriver.java
@@ -1,9 +1,41 @@
 package queries;
 
+
+import java.io.Console;
+
+import common.Utility;
+
 public class QueryDriver {
 
-    public static void run() {
+    Console console;
+
+    public QueryDriver(){
+        console = System.console();
+    }
+
+    public void run() {
         
+        String single_or_transaction = Utility.enter_in_console("Select option 1/2/3: \n"
+        +"1. Single Query\n"
+        +"2. Transaction\n"
+        +"3. See Query Guide", console);
+
+        if(single_or_transaction.equals("1")){
+            single_query_execution();
+        }
+        else if(single_or_transaction.equals("2")){
+            transaction_execution();
+        }
+        else if(single_or_transaction.equals("3")){
+            Guide.print_queries_for_reference();
+            run();
+        }
+    }
+
+    private void transaction_execution() {
+    }
+
+    private void single_query_execution() {
     }
     
 }
