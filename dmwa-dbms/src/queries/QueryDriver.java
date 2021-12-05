243f0c5b3bd1933ccd03cb5c1d959cd89aed717b
Prachi Kabtiyal <pr522601@dal.ca>
2021-12-05 13:11:34 -0400

    single query removed

--- dmwa-dbms/src/queries/QueryDriver.java
+++ dmwa-dbms/src/queries/QueryDriver.java
@@ -20,31 +20,44 @@ public class QueryDriver {
     public void run() {
         
         String single_or_transaction = Utility.enter_in_console("Select option 1/2/3: \n"
-        +"1. Single Query\n"
-        +"2. Transaction\n"
-        +"3. See Query Guide", console);
+        +"1. Query\n"
+        +"2. See Query Guide", console);
 
         if(single_or_transaction.equals("1")){
-            single_query_execution();
+            query_execution();
         }
+        // else if(single_or_transaction.equals("2")){
+        //     transaction_execution();
+        // }
         else if(single_or_transaction.equals("2")){
-            transaction_execution();
-        }
-        else if(single_or_transaction.equals("3")){
             Guide.print_queries_for_reference();
             run();
         }
     }
 
-    private void single_query_execution() {
+    private void query_execution() {
 
-        String query = Utility.enter_in_console("Enter query or press 1 to go back to menu: ", console);
-        QueryIdentifier queryIdentifier= new QueryIdentifier(query, user);
-        while(!query.equals("1")){
-            queryIdentifier.run();
-            query = Utility.enter_in_console("Enter query or press 1 to go back to menu: ", console);
-            queryIdentifier.setQuery(query);
+        String query="";
+        QueryIdentifier queryIdentifier= new QueryIdentifier(query, user);;
+
+        do{
+            query= Utility.enter_in_console("Enter query or press 1 to go back to previous menu and 2 to go back to main menu:", console);
+            if(query.equals("1")){
+                run();
+            }
+            if(query.equals("2")){
+                break;
+            }
+            if(!query.toLowerCase().startsWith("start trasaction")){
+                queryIdentifier.setQuery(query);
+                queryIdentifier.run();
+                continue;
+            }
+            else{
+                //transaction_execution
+            }
         }
+        while(!query.equals("1") && !query.equals("2"));
         
     }
 
